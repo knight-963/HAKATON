da902bd69070996e84064a428fb1f92b29840d24
Doniyor <doniyormurodov246810@gmail.com>
2024-10-27 14:37:06 +0500

    commit

---
+++ Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css"
+
+export default function Navbar() {
+    return (
+      <div className="nv">
+
+<nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/"><img src="p1.jpg" className="h-[60px]" alt="" /></Link>
+        </div>
+      <ul className={styles.navLinks}>
+        <li><Link href="/">Bosh Sahifa</Link></li>
+        <li><Link href="/About">Muammo Tahlili</Link></li>
+        <li><Link href="/Yechimlar">Yechimlar</Link></li>
+        <li><Link href="/events">Tadbirlar</Link></li>
+        <li><Link href="/resources">Resurslar</Link></li>
+        <li><Link href="/collaboration">Hamkorlik</Link></li>
+        <li><Link href="/contact">Aloqa</Link></li>
+      </ul>
+    </nav>
+        
+      </div>
+    );
+  }
---
+++ pages/Yechimlar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Yechimlar = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Yechimlar
