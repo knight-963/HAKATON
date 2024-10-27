8049f08b4031db8fbaf8e945285acb60ddddcc53
Doniyor <doniyormurodov246810@gmail.com>
2024-10-27 15:26:44 +0500

    ss

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,9 +3,10 @@ import styles from "../styles/Navbar.module.css"
 
 export default function Navbar() {
     return (
-      <div className="nv">
+      <div className="flex justify-center">
 
-<nav className={styles.navbar}>
+        <div className={styles.nvn}>
+        <nav className={styles.navbar}>
       <div className={styles.logo}>
         <Link href="/"><img src="p1.jpg" className="h-[60px]" alt="" /></Link>
         </div>
@@ -13,12 +14,10 @@ export default function Navbar() {
         <li><Link href="/">Bosh Sahifa</Link></li>
         <li><Link href="/About">Muammo Tahlili</Link></li>
         <li><Link href="/Yechimlar">Yechimlar</Link></li>
-        <li><Link href="/events">Tadbirlar</Link></li>
-        <li><Link href="/resources">Resurslar</Link></li>
-        <li><Link href="/collaboration">Hamkorlik</Link></li>
-        <li><Link href="/contact">Aloqa</Link></li>
+        <li><Link href="/Register">Aloqa</Link></li>
       </ul>
     </nav>
+        </div>
         
       </div>
     );
