9206243825f304f98117d1ac52ed98be3e7e9863
Doniyor <doniyormurodov246810@gmail.com>
2024-10-27 15:49:13 +0500

    Committ

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,24 +1,24 @@
-import Link from "next/link";
-import styles from "../styles/Navbar.module.css"
+// import Link from "next/link";
+// import styles from "../styles/Navbar.module.css"
 
-export default function Navbar() {
-    return (
-      <div className="flex justify-center">
+// export default function Navbar() {
+//     return (
+//       <div className="flex justify-center">
 
-        <div className={styles.nvn}>
-        <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Link href="/"><img src="p1.jpg" className="h-[60px]" alt="" /></Link>
-        </div>
-      <ul className={styles.navLinks}>
-        <li><Link href="/">Bosh Sahifa</Link></li>
-        <li><Link href="/About">Muammo Tahlili</Link></li>
-        <li><Link href="/Yechimlar">Yechimlar</Link></li>
-        <li><Link href="/Register">Aloqa</Link></li>
-      </ul>
-    </nav>
-        </div>
+//         <div className={styles.nvn}>
+//         <nav className={styles.navbar}>
+//       <div className={styles.logo}>
+//         <Link href="/"><img src="p1.jpg" className="h-[60px]" alt="" /></Link>
+//         </div>
+//       <ul className={styles.navLinks}>
+//         <li><Link href="/">Bosh Sahifa</Link></li>
+//         <li><Link href="/About">Muammo Tahlili</Link></li>
+//         <li><Link href="/Yechimlar">Yechimlar</Link></li>
+//         <li><Link href="/Register">Aloqa</Link></li>
+//       </ul>
+//     </nav>
+//         </div>
         
-      </div>
-    );
-  }
+//       </div>
+//     );
+//   }
